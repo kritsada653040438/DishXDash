79fdd157822a44bb94673ddb959f4657b06ff214
kritsada6530404386 <kritsada.ma@kkumail.com>
2024-09-22 20:51:18 +0700

    Quick-Eats-Pages

--- src/app/components/Footer.jsx
+++ src/app/components/Footer.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 function Footer() {
   return (
     <footer className='bg-[#333] justify-center items-center p-3 text-white text-center'>
-        <p>Copyright 2024.</p>
+        <p>DishXDash 2024.</p>
     </footer>
   )
 }
--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,31 +1,33 @@
 "use client";
 
-import Image from "next/image";
 import Container from "./components/Container";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
-import NextLogo from '../../public/custom-logo.png';
 import { useSession } from "next-auth/react";
+import Link from "next/link";
 
 export default function Home() {
-  
   const { data: session } = useSession();
 
   return (
-    <main className="bg-gray-50 min-h-screen flex flex-col">
+    <main className="bg-white min-h-screen flex flex-col">
       <Container>
         <Navbar session={session} />
         <div className="flex-grow flex flex-col items-center justify-center text-center p-6">
-          <h3 className="text-5xl font-bold text-red-800">Welcome to DishDash!</h3>
-          <p className="text-xl mt-4 text-gray-700 max-w-2xl mx-auto">
+          <h3 className="text-6xl font-bold text-black mb-4">
+            Welcome to DishXDash!
+          </h3>
+          <p className="text-lg text-gray-600 max-w-2xl mx-auto leading-relaxed">
             Discover quick and easy meal options that fit your busy lifestyle. Our platform allows you to explore a variety of delicious dishes, perfect for any occasion. Whether you're looking for a quick breakfast, a satisfying lunch, or a delightful dinner, we've got you covered!
           </p>
-          
+
           {/* Call to Action Button */}
           <div className="mt-8">
-            <button className="px-8 py-3 bg-blue-500 text-white rounded-lg shadow-md hover:bg-blue-600 transition duration-200">
-              Start Exploring
-            </button>
+            <Link href="/discover">
+              <button className="px-8 py-3 bg-gray-900 text-white rounded-full shadow-md hover:bg-gray-700 transition duration-200 transform hover:scale-105">
+                Start eating!
+              </button>
+            </Link>
           </div>
         </div>
         <Footer />
